72552aa610fb8e132586951441a23a3713fbe0e0
Gorka Otermin <gorkaotba@opendeusto.es>
2018-11-04 20:56:54 +0100

    Commit for pulling master

--- src/test/java/grupo2/AppTest.java
+++ src/test/java/grupo2/AppTest.java
@@ -1,6 +1,6 @@
 package grupo2;
 
-import static junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
