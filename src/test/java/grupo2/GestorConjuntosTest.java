37ec305b6ade8e3f52cea46283690a19ebd84fa8
jonalonsov <jonalonso@opendeusto.es>
2018-11-30 18:52:44 +0100

    junit

--- src/test/java/grupo2/GestorConjuntosTest.java
+++ src/test/java/grupo2/GestorConjuntosTest.java
@@ -42,6 +42,9 @@ public class GestorConjuntosTest {
 		setUpIsDone = true;
     }
 	
+	
+	
+	
 	 @Test
 	    public void testanyadirConjunto()
 	    {
@@ -146,6 +149,197 @@ public class GestorConjuntosTest {
 	    
 	    	//assertTrue( conjunto == "leire" );
 	    
+	    }
+	 
+	 @Test
+	    public void testcrearAleatorio1()
+	    {
+			GestorConjuntos gc = new GestorConjuntos(); 
+			GestorPrendas gp = new GestorPrendas();
+			
+			Prenda prenda1 = new Prenda ( '0', "Pantalon largo", "Negro", "Algodón", "/", "j");
+			
+			
+			gp.anyadirPrenda( BaseDeDatos.getStatement(), prenda1);
+			
+	    	int exists = gc.crearAleatorio1();
+	    	assertTrue( exists != 0 );
+	    	
+	    }
+		
+		@Test
+	    public void testcrearAleatorio2()
+	    {
+			GestorConjuntos gc = new GestorConjuntos(); 
+			GestorPrendas gp = new GestorPrendas();
+			
+			Prenda prenda2 = new Prenda ( '0', "Jersey", "Negro", "Algodón", "/", "j");
+			
+			
+			gp.anyadirPrenda( BaseDeDatos.getStatement(), prenda2);
+	    	int exists = gc.crearAleatorio2();
+	    	
+	    	assertTrue( exists != 0 );
+	    	
+	    }
+		
+		@Test
+	    public void testcrearLluvia1()
+	    {
+			GestorConjuntos gc = new GestorConjuntos(); 
+			GestorPrendas gp = new GestorPrendas();
+			
+			Prenda prenda3 = new Prenda ( '0', "Pantalon largo", "Negro", "Algodón", "/", "j");
+			
+			
+			gp.anyadirPrenda( BaseDeDatos.getStatement(), prenda3);
+	    	int exists = gc.crearLluvia1();
+	    	
+	    	assertTrue( exists != 0 );
+	    	
+	    }
+		
+		@Test
+	    public void testcrearLluvia2()
+	    {
+			GestorConjuntos gc = new GestorConjuntos(); 
+			GestorPrendas gp = new GestorPrendas();
+			
+			Prenda prenda4 = new Prenda ( '0', "Chaqueta", "Negro", "impermeable", "/", "j");
+			
+			
+			gp.anyadirPrenda( BaseDeDatos.getStatement(), prenda4); 
+	    	int exists = gc.crearLluvia2();
+	    	
+	    	assertTrue( exists != 0 );
+	    	
+	    }
+		
+		@Test
+	    public void testcrearCalor1()
+	    {
+			GestorConjuntos gc = new GestorConjuntos(); 
+			GestorPrendas gp = new GestorPrendas();
+			
+			Prenda prenda5 = new Prenda ( '0', "falda", "Negro", "Algodón", "/", "j");
+			
+			
+			gp.anyadirPrenda( BaseDeDatos.getStatement(), prenda5); 
+	    	int exists = gc.crearCalor1();
+	    	
+	    	assertTrue( exists != 0 );
+	    	
+	    }
+		
+		@Test
+	    public void testcrearCalor2()
+	    {
+			GestorConjuntos gc = new GestorConjuntos(); 
+			GestorPrendas gp = new GestorPrendas();
+			
+			Prenda prenda6 = new Prenda ( '0', "polo", "Negro", "Algodón", "/", "j");
+			
+			
+			gp.anyadirPrenda( BaseDeDatos.getStatement(), prenda6); 
+	    	int exists = gc.crearCalor2();
+	    	
+	    	assertTrue( exists != 0 );
+	    	
+	    }
+		
+	    public void testcrearFrio1()
+	    {
+	    	GestorConjuntos gc = new GestorConjuntos(); 
+			GestorPrendas gp = new GestorPrendas();
+			
+			Prenda prenda7 = new Prenda ( '0', "Pantalon largo", "Negro", "Algodón", "/", "j");
+			
+			
+			gp.anyadirPrenda( BaseDeDatos.getStatement(), prenda7);
+	    	int exists = gc.crearFrio1();
+	    	
+	    	assertTrue( exists != 0 );
+	    	
+	    }
+		
+		@Test
+	    public void testcrearFrio2()
+	    {
+			GestorConjuntos gc = new GestorConjuntos(); 
+			GestorPrendas gp = new GestorPrendas();
+			
+			Prenda prenda8 = new Prenda ( '0', "Camiseta larga", "Negro", "Algodón", "/", "j");
+			
+			
+			gp.anyadirPrenda( BaseDeDatos.getStatement(), prenda8);
+	    	int exists = gc.crearFrio2();
+	    	
+	    	assertTrue( exists != 0 );
+	    	
+	    }
+		
+		@Test
+	    public void testcrearUrbano1()
+	    {
+			GestorConjuntos gc = new GestorConjuntos(); 
+			GestorPrendas gp = new GestorPrendas();
+			
+			Prenda prenda9 = new Prenda ( '0', "Pantalon largo", "Negro", "Algodón", "/", "j");
+			
+			
+			gp.anyadirPrenda( BaseDeDatos.getStatement(), prenda9);
+	    	int exists = gc.crearUrbano1();
+	    	
+	    	assertTrue( exists != 0 );
+	    	
+	    }
+		
+		@Test
+	    public void testcrearUrbano2()
+	    {
+			GestorConjuntos gc = new GestorConjuntos(); 
+			GestorPrendas gp = new GestorPrendas();
+			
+			Prenda prenda10 = new Prenda ( '0', "Camiseta tirantes", "Negro", "Algodón", "/", "j");
+			
+			
+			gp.anyadirPrenda( BaseDeDatos.getStatement(), prenda10);
+	    	int exists = gc.crearUrbano2();
+	    	
+	    	assertTrue( exists != 0 );
+	    	
+	    }
+		
+		@Test
+	    public void testcrearFormal1()
+	    {
+			GestorConjuntos gc = new GestorConjuntos(); 
+			GestorPrendas gp = new GestorPrendas();
+			
+			Prenda prenda11 = new Prenda ( '0', "Pantalon largo", "Negro", "Algodón", "/", "j");
+			
+			
+			gp.anyadirPrenda( BaseDeDatos.getStatement(), prenda11);
+	    	int exists = gc.crearFormal1();
+	    	
+	    	assertTrue( exists != 0 );
+	    	
+	    }
+		
+		@Test
+	    public void testcrearFormal2()
+	    {
+			GestorConjuntos gc = new GestorConjuntos(); 
+			GestorPrendas gp = new GestorPrendas();
+			
+			Prenda prenda12 = new Prenda ( '0', "Polo", "Blanco", "Algodón", "/", "j");
+			
+			
+			gp.anyadirPrenda( BaseDeDatos.getStatement(), prenda12); 
+	    	int exists = gc.crearFormal2();
+	    	
+	    	assertTrue( exists != 0 );
+	    	
 	    }
 
 }
